fb644b10052a23cec0033c6298f1012c8c7035d3
Amirali Gorji <amirali.gorji@gmail.com>
2024-09-04 05:27:27 +0330

    :sparkles: feat: Add shop models

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,1 +1,53 @@
+from django.db import models
+
+from apps.user.models import CustomUser
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100, null=True)
+    category = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Address(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    main_avenue = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    other_desc = models.CharField(max_length=100)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+    address = models.ForeignKey(Address, on_delete=models.PROTECT)
+    is_reminded = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+    price = models.CharField(max_length=100, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Viewpoint(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    content_text = models.TextField(null=True, blank=True)
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 
