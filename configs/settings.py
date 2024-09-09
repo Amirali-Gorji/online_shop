12647f30611d731c03f3c6949d6bc85a93ff5b49
Amirali Gorji <amirali.gorji@gmail.com>
2024-09-09 18:46:50 +0330

    :sparkles: feat: Change db to postgresql

--- configs/settings.py
+++ configs/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGIN'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS')
     }
 }
 
