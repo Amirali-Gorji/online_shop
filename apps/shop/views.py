2d8bfa0b61dc2e1f2b130b42d55670544e758161
Amirali Gorji <amirali.gorji@gmail.com>
2024-09-10 11:29:56 +0330

    :sparkles: feat: Add create product api

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from apps.shop.serializers import (
+    AddProductToCartSerializer,
+)
+class CreateProductAPI(APIView):
+    authentication_classes = []
+    permission_classes = []
+    required_permissions = {'post':'can_add_product'}
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer['name']
+            city = serializer['city']
+            category = serializer['category']
+            price = serializer['price']
+            product = ProductService.create_product(name=name, city=city, category=category, price=price)
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
