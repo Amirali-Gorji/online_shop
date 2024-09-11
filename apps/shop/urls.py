971a2864d8fe1a85257907c5c87ddf0d43d3593f
Amirali Gorji <amirali.gorji@gmail.com>
2024-09-11 20:06:15 +0330

    :sparkles: feat: Add shop app urls

---
+++ apps/shop/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+
+from apps.shop.views import (
+    CreateViewPointAPI,
+    DeleteViewPointAPI,
+    ListViewPointAPI,
+    CreateProductAPI, 
+    DeleteProductAPI,
+    AddProductToCartAPI,
+    # RemoveProductFromCartAPI,
+    CreateCategoryAPI, 
+    ListProductAPI, 
+    ListCategoryAPI,
+    UpdateProductAPI,
+    ListAddressAPI,
+    CreateAddressAPI
+)
+
+
+urlpatterns = [
+    path('products/', ListProductAPI.as_view(), name='list-products'),
+    path('products/create/', CreateProductAPI.as_view(), name='create-product'),
+    path('products/update/', UpdateProductAPI.as_view(), name='update-products'),
+    path('products/<int:product_id>/delete/', DeleteProductAPI.as_view(), name='delete-product'),
+    path('products/<int:product_id>/viewpoints/', ListViewPointAPI.as_view(), name='list-product-viewpoints'),
+    path('products/<int:product_id>/viewpoints/create/', CreateViewPointAPI.as_view(), name='create-product-viewpoint'),
+    path('products/<int:product_id>/viewpoints/<int:viewpoint_id>/delete/', DeleteViewPointAPI.as_view(), name='delete-viewpoint'),
+    path('cart/add/', AddProductToCartAPI.as_view(), name='add-product-to-cart'),
+    # path('cart/remove/', RemoveProductFromCartAPI.as_view(), name='remove-product-from-cart'),
+    # path('cart/payment/', .as_view(), name='pay-cart'),
+    path('categories/', ListCategoryAPI.as_view(), name='list-categories'),
+    path('categories/create/', CreateCategoryAPI.as_view(), name='create-categories'),
+    path('address/', ListAddressAPI.as_view(), name='list-address'),
+    path('address/create/', CreateAddressAPI.as_view(), name='create-address'),
+]
+
