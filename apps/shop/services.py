f46e3a0781b7f88a4e9bb69c3fd9c15d9429fd6b
Amirali Gorji <amirali.gorji@gmail.com>
2024-09-11 20:03:36 +0330

    :sparkles: feat: Add product service

--- apps/shop/services.py
+++ apps/shop/services.py
@@ -7,6 +7,43 @@ from apps.shop.models import (
     Category
 )
 
+class ProductService:
+    @staticmethod
+    def create_product(*, name, city, category, price):
+        product = Product.objects.create(name=name, price=price)
+        if product:
+            product.category.add(*category)   
+            product.city.add(*city)
+        return product
+
+
+    @staticmethod
+    def delete_product(*, product=None):
+        product.category.clear()
+        product.city.clear()
+        product.delete()
+
+
+    @staticmethod
+    def update_product(*, product=None, name=None, city=None, category=None, price=None):
+        update_product = False
+        if name:
+            product.name = name
+            update_product = True
+        if city:
+            product.city.clear()
+            product.city.add(*city)
+            update_product = True
+        if category:
+            product.category.clear()
+            product.category.add(*category)
+            update_product = True
+        if price:
+            product.price = price
+            update_product = True
+        if update_product:
+            product.save()
+        return product
 class ViewPointService:
     @staticmethod
     def add_viewpoint(*, user_id, product_id, score, content_text=None):
